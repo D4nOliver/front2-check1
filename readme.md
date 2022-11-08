66a016b49b35c60f101ab7e3036ff8cdbd26e9db
unknown <danieloliversilva9@gmail.com>
2022-11-08 13:16:10 -0300

    1st commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Se mudarem algo deixa um resuminho aki
+
+# Integrantes
+
+- Daniel O. da silva
+- Lucas Soares
---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+let cardTitulo = document.getElementById("cardTitulo");
+let cardImg = document.getElementById("cardImg");
+let cardDesc = document.getElementById("cardDesc");
+let cardSubmit = document.getElementById("cardSubmit");
+
+console.log(cardTitulo.value);
